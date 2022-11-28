d9bdd471fedf577166a57cd68d51387abb6078bb
nateosborne <natedresdeno@gmail.com>
2022-11-28 11:24:45 -0500

    login functions

--- app/app.js
+++ app/app.js
@@ -1,18 +1,23 @@
 import * as MODEL from "./model.js";
 
 function changeRoute() {
-let hashTag = window.location.hash;
- let pageID = hashTag.replace('#', '');
-//   console.log(hashTag + ' ' + pageID);
-MODEL.changePage(pageID);
-
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#", "");
+  //   console.log(hashTag + ' ' + pageID);
+  MODEL.changePage(pageID, initSubmitListener);
 }
 
 function initURLListener() {
-$(window).on('hashchange', changeRoute);
-changeRoute();
+  $(window).on("hashchange", changeRoute);
+  changeRoute();
+}
+
+function initSubmitListener() {
+  $("#loginbutton").on("click", function (e) {
+    console.log("submit");
+  });
 }
 
 $(document).ready(function () {
-initURLListener();
+  initURLListener();
 });
--- app/model.js
+++ app/model.js
@@ -1,13 +1,19 @@
-export function changePage(pageID){
-    if (pageID != '') {
-        $.get(`pages/${pageID}.html`, function (data) {
-         console.log('data ' + data);
-         $('#app').html(data);
-        });
-        } else {
-        $.get(`pages/home.html`, function (data) {
-        console.log('data ' + data);
-         $('#app').html(data);
-        });
-        }
+export function changePage(pageID, callback) {
+  if (pageID != "") {
+    $.get(`pages/${pageID}.html`, function (data) {
+      console.log("data " + data);
+      $("#app").html(data);
+    });
+  } else if (pageID == "account") {
+    $.get(`pages/${pageID}.html`, function (data) {
+      console.log("data " + data);
+      $("#app").html(data);
+    });
+  } else {
+    $.get(`pages/home.html`, function (data) {
+      console.log("data " + data);
+      $("#app").html(data);
+      callback();
+    });
+  }
 }
