8a0be18a2beee4c1245a05523559a24f16824889
nateosborne <natedresdeno@gmail.com>
2022-12-05 10:34:34 -0500

    Added more to the login function. Sign up is working

--- app/app.js
+++ app/app.js
@@ -13,16 +13,50 @@ function initURLListener() {
 }
 
 function initSubmitListener() {
+  var userObj = {};
   $("#loginbutton").on("click", function (e) {
     console.log("submit");
     let em = $("#email").val();
     let pw = $("#pw").val();
-    console.log(`${em}${pw}`);
+    // console.log(`${em}${pw}`);
     if (em == "") {
-      alert("Please enter your email");
+      alert("Please enter your Email");
+    } else if (pw == "") {
+      alert("Please enter your Password");
+    } else {
+      alert("You have logged in!");
     }
-    if (pw == "") {
-      alert("Please enter your password");
+
+    if (em == userObj[2] && pw == userObj[3]) {
+      alert("You have logged in!");
+      console.log("login");
+    }
+  });
+  $("#signupbutton").on("click", function (e) {
+    console.log("submit");
+    let fn = $("#fn").val();
+    let ln = $("#ln").val();
+    let em = $("#email2").val();
+    let pw = $("#pw2").val();
+    // console.log(`${em}${pw}`);
+    if (fn == "") {
+      alert("Please enter your First Name");
+    } else if (ln == "") {
+      alert("Please enter your Last Name");
+    } else if (em == "") {
+      alert("Please enter your Email");
+    } else if (pw == "") {
+      alert("Please enter your Password");
+    } else {
+      let userObj = {
+        firstName: fn,
+        lastName: ln,
+        email: em,
+        password: pw,
+      };
+      alert("You have signed up!");
+
+      MODEL.setUserInfo(userObj);
     }
   });
 }
@@ -30,5 +64,3 @@ function initSubmitListener() {
 $(document).ready(function () {
   initURLListener();
 });
-
-
--- app/model.js
+++ app/model.js
@@ -1,3 +1,5 @@
+var userInfo = {};
+
 export function changePage(pageID, callback) {
   if (pageID != "") {
     $.get(`pages/${pageID}.html`, function (data) {
@@ -10,10 +12,16 @@ export function changePage(pageID, callback) {
       $("#app").html(data);
     });
   } else {
+    //login only works when "home.html" is "login.html"
     $.get(`pages/home.html`, function (data) {
       console.log("data " + data);
       $("#app").html(data);
       callback();
     });
   }
+}
+
+export function setUserInfo(userObject) {
+  userInfo = userObject;
+  console.log(userInfo);
 }
